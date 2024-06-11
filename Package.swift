92a7864dbaaf9951543d80bc08ab20640178410d
runner <runner@Mac-1718085944114.local>
2024-06-11 06:58:35 +0000

    [CI] Publish version 1.0.11

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://maven.pkg.github.com/zawadz88/AnalyticsEventGeneratorSample-SharedLibrary/dev/zawadzki/sharedanalyticslibrary/shared-analytics-library-kmmbridge/1.0.10/shared-analytics-library-kmmbridge-1.0.10.zip"
-let remoteKotlinChecksum = "84d7d828504bdb34f466f4bd2736fecc2c0386406a00e8d09613fea283906c4e"
+let remoteKotlinUrl = "https://maven.pkg.github.com/zawadz88/AnalyticsEventGeneratorSample-SharedLibrary/dev/zawadzki/sharedanalyticslibrary/shared-analytics-library-kmmbridge/1.0.11/shared-analytics-library-kmmbridge-1.0.11.zip"
+let remoteKotlinChecksum = "f9a8f8fe409e03e6c2f66a6da981f06dcf6a6415d0440ceb668a114030b097af"
 let packageName = "SharedAnalyticsLibrary"
 // END KMMBRIDGE BLOCK
 
